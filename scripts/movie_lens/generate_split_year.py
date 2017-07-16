aa0597a7434d9636bc0cc588ce6e4452d45e392b
gennei <sai.gennei@gmail.com>
2017-07-16 23:32:12 +0900

    生成するところまで

--- scripts/movie_lens/generate_split_year.py
+++ scripts/movie_lens/generate_split_year.py
@@ -6,11 +6,15 @@ path = './datasets/movie_lens/ml-latest-small/movies.csv'
 with open(path, 'r') as f:
     all    = csv.reader(f)
     header = next(all)  # ヘッダーを読み飛ばしたい時
-    print(header)
+    header.append("year")
+    print(",".join(header))
 
     for row in all:
         title = row[1]
-        year = re.search("\(\d{4}\)", title)
-        if year == None
-        else:
-            print(year.group()[1:-1])
+        match = re.search("\(\d{4}\)", title)
+        if match is not None:
+            row[1] = row[1].replace(match.group(), "")
+            row.append(match.group()[1:-1])
+
+        txt = ",".join(row)
+        print(txt)

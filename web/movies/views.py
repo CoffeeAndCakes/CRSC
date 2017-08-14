ecbc43cf4b42f68c06eca83a0767bf44c4c438af
gennei <sai.gennei@gmail.com>
2017-08-14 22:50:15 +0900

    映画の詳細ページを追加

--- web/movies/views.py
+++ web/movies/views.py
@@ -1,6 +1,14 @@
+from django.http import Http404
 from django.shortcuts import render
 from .models import Movie
 
 def index(request):
     movies = Movie.objects.all()
     return  render(request, 'movies/index.html', {'movies': movies})
+
+def detail(request, movie_id):
+    try:
+        movie = Movie.objects.get(pk=movie_id)
+    except Movie.DoesNotExist:
+        raise Http404("movie does not exist")
+    return render(request, 'movies/detail.html', {'movie': movie})

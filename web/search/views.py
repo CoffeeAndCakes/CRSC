a4a04cea25296b69d9e8f5104928c813f6434365
gennei <sai.gennei@gmail.com>
2017-08-19 14:53:22 +0900

    年代別映画のテンプレート作成

--- web/search/views.py
+++ web/search/views.py
@@ -1,8 +1,18 @@
 from django.shortcuts import render
-from movies.models import Genre
+from movies.models import Movie, Genre
 
 def index(request):
     genres = Genre.objects.all()
     context = {'genres': genres}
 
-    return  render(request, 'search/index.html', context)
+    return render(request, 'search/index.html', context)
+
+def years_result(request, year):
+    movies = Movie.objects.filter(year=year)
+
+    context = {
+        'movies': movies,
+        'year': year
+    }
+
+    return render(request, 'search/years.html', context)
